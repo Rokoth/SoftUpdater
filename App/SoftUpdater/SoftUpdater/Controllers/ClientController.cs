a87af893040dbadee17418d71c928239e1ea0c4e
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-05-12 12:08:03 +0500

    -refac -techtask

--- App/SoftUpdater/SoftUpdater/Controllers/ClientController.cs
+++ App/SoftUpdater/SoftUpdater/Controllers/ClientController.cs
@@ -1,31 +1,52 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref 1
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using SoftUpdater.Contract.Model;
 using SoftUpdater.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace SoftUpdater.Controllers
 {
+    /// <summary>
+    /// Client controller
+    /// </summary>
     public class ClientController : Controller
     {
         private readonly IServiceProvider _serviceProvider;
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="serviceProvider"></param>
         public ClientController(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
         // GET: ClientController
+        /// <summary>
+        /// List method
+        /// </summary>
+        /// <returns></returns>
         [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// paged partial view
+        /// </summary>
+        /// <param name="page">page number from 0</param>
+        /// <param name="size">page size</param>
+        /// <param name="sort">sorting</param>
+        /// <param name="name">name filter</param>
+        /// <returns></returns>
         [Authorize]
         public async Task<ActionResult> ListPaged(int page = 0, int size = 10, string sort = null, string name = null)
         {
