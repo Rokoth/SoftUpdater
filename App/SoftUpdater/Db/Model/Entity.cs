9b064cf94c8b636311983db881bc20c2ef25f26e
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-06-18 00:10:34 +0500

    Add Release model

--- App/SoftUpdater/Db/Model/Entity.cs
+++ App/SoftUpdater/Db/Model/Entity.cs
@@ -97,19 +97,6 @@ namespace SoftUpdater.Db.Model
         public string BasePath { get; set; }
     }
 
-    [TableName("release")]
-    public class Release : Entity
-    {        
-        [ColumnName("path")]
-        public string Path { get; set; }
-        [ColumnName("version")]
-        public string Version { get; set; }
-        [ColumnName("number")]
-        public int Number { get; set; }       
-        [ColumnName("client_id")]
-        public Guid ClientId { get; set; }       
-    }
-
     [TableName("h_release")]
     public class ReleaseHistory : EntityHistory
     {
---
+++ App/SoftUpdater/Db/Model/Release.cs
@@ -0,0 +1,18 @@
+﻿using SoftUpdater.Db.Attributes;
+using System;
+
+namespace SoftUpdater.Db.Model
+{
+    [TableName("release")]
+    public class Release : Entity
+    {        
+        [ColumnName("path")]
+        public string Path { get; set; }
+        [ColumnName("version")]
+        public string Version { get; set; }
+        [ColumnName("number")]
+        public int Number { get; set; }       
+        [ColumnName("client_id")]
+        public Guid ClientId { get; set; }       
+    }
+}
