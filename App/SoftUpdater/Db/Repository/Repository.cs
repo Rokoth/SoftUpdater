bde1cb3d7c5b6e12b2aa3bb8a0f95358a3f1408c
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-07-09 20:18:53 +0500

    Repository refactoring

--- App/SoftUpdater/Db/Repository/Repository.cs
+++ App/SoftUpdater/Db/Repository/Repository.cs
@@ -1,25 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//Copyright 2021 Dmitriy Rokoth
+//Licensed under the Apache License, Version 2.0
+//
+//ref 1
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SoftUpdater.Db.Context;
 using SoftUpdater.Db.Interface;
 using SoftUpdater.Db.Model;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Linq.Dynamic.Core;
 
 namespace SoftUpdater.Db.Repository
 {
+    /// <summary>
+    /// Repository - wrapper for db works
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Repository<T> : IRepository<T> where T : class, IEntity 
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger _logger;
 
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="serviceProvider"></param>
         public Repository(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
@@ -37,19 +46,26 @@ namespace SoftUpdater.Db.Repository
         {
             return await ExecuteAsync(async (context) => {
                 var item = context.Set<T>().Add(entity).Entity;
-                if (withSave) await context.SaveChangesAsync();
+                await Task.CompletedTask;
                 return item;
-            }, "AddAsync");
+            }, "AddAsync", withSave);
         }
-               
+
+        /// <summary>
+        /// Метод удаления из базы
+        /// </summary>
+        /// <param name="entity">модель</param>
+        /// <param name="withSave">с сохраннеием</param>
+        /// <param name="token">токен</param>
+        /// <returns></returns>
         public async Task<T> DeleteAsync(T entity, bool withSave, CancellationToken token)
         {
             return await ExecuteAsync(async (context) => {
                 entity.IsDeleted = true;
-                var item = context.Set<T>().Update(entity).Entity;
-                if (withSave) await context.SaveChangesAsync();
+                var item = context.Set<T>().Update(entity).Entity;                
+                await Task.CompletedTask;
                 return item;
-            }, "DeleteAsync");
+            }, "DeleteAsync", withSave);
         }
 
         /// <summary>
@@ -60,8 +76,26 @@ namespace SoftUpdater.Db.Repository
         /// <returns>список моделей</returns>
         public async Task<Contract.Model.PagedResult<T>> GetAsync(Filter<T> filter, CancellationToken token)
         {
-            return await ExecuteAsync(async (context) => {
-                var all = context.Set<T>().Where(s=>!s.IsDeleted).Where(filter.Selector);
+            return await GetAsyncInternal(filter, false, "GetAsync");
+        }
+
+        /// <summary>
+        /// Метод получения отфильтрованного списка моделей с постраничной отдачей (с удаленными записями)
+        /// </summary>
+        /// <param name="filter">фильтр</param>
+        /// <param name="token">токен</param>
+        /// <returns>список моделей</returns>
+        public async Task<Contract.Model.PagedResult<T>> GetAsyncDeleted(Filter<T> filter, CancellationToken token)
+        {
+            return await GetAsyncInternal(filter, true, "GetAsyncDeleted");
+        }
+
+        private async Task<Contract.Model.PagedResult<T>> GetAsyncInternal(Filter<T> filter, bool withDeleted, string methodName)
+        {
+            return await ExecuteAsync(async (context) =>
+            {
+                var all = context.Set<T>().Where(filter.Selector);
+                if(!withDeleted) all = all.Where(s => !s.IsDeleted);
                 if (!string.IsNullOrEmpty(filter.Sort))
                 {
                     all = all.OrderBy(filter.Sort);
@@ -73,8 +107,8 @@ namespace SoftUpdater.Db.Repository
                 var count = await all.CountAsync();
                 var pageCount = Math.Max(((count % filter.Size) == 0) ? (count / filter.Size) : ((count / filter.Size) + 1), 1);
                 return new Contract.Model.PagedResult<T>(result, pageCount);
-            }, "GetAsync");
-        }
+            }, methodName, false);
+        }        
 
         /// <summary>
         /// Метод получения модели по id
@@ -87,93 +121,62 @@ namespace SoftUpdater.Db.Repository
             return await ExecuteAsync(async (context) => {
                 return await context.Set<T>()
                     .Where(s => !s.IsDeleted && s.Id == id).FirstOrDefaultAsync();
-            }, "GetAsync");
+            }, "GetAsync", false);
         }
 
-        public async Task<Contract.Model.PagedResult<T>> GetAsyncDeleted(Filter<T> filter, CancellationToken token)
-        {
-            return await ExecuteAsync(async (context) => {
-                var all = context.Set<T>().Where(filter.Selector);
-                if (!string.IsNullOrEmpty(filter.Sort))
-                {
-                    all = all.OrderBy(filter.Sort);
-                }
-                var result = await all
-                    .Skip(filter.Size * filter.Page)
-                    .Take(filter.Size)
-                    .ToListAsync();
-                var count = await all.CountAsync();
-                var pageCount = Math.Max(((count % filter.Size) == 0) ? (count / filter.Size) : ((count / filter.Size) + 1),1);
-                return new Contract.Model.PagedResult<T>(result, pageCount);
-            }, "GetAsyncDeleted");
-        }
+        
 
+        /// <summary>
+        /// Метод получения модели по id (в тч удаленные)
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="token">token</param>
+        /// <returns></returns>
         public async Task<T> GetAsyncDeleted(Guid id, CancellationToken token)
         {
             return await ExecuteAsync(async (context) => {
                 return await context.Set<T>()
                     .Where(s => s.Id == id).FirstOrDefaultAsync();
-            }, "GetAsync");
+            }, "GetAsync", false);
         }
 
+        /// <summary>
+        /// Метод обновления записи в базе
+        /// </summary>
+        /// <param name="entity">модель</param>
+        /// <param name="withSave">с сохраннеием</param>
+        /// <param name="token">токен</param>
+        /// <returns></returns>
         public async Task<T> UpdateAsync(T entity, bool withSave, CancellationToken token)
         {
             return await ExecuteAsync(async (context) => {                
                 var item = context.Set<T>().Update(entity).Entity;
-                if (withSave) await context.SaveChangesAsync();
+                await Task.CompletedTask;
                 return item;
-            }, "UpdateAsync");
+            }, "UpdateAsync", withSave);
         }
 
-        private async Task<TEx> ExecuteAsync<TEx>(Func<DbPgContext, Task<TEx>> action, string method)
+        /// <summary>
+        /// Обертка выполнения запросов (обработка ошибок)
+        /// </summary>
+        /// <typeparam name="TEx"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        private async Task<TEx> ExecuteAsync<TEx>(Func<DbPgContext, Task<TEx>> action, string method, bool withSave)
         {
             try
             {
                 var context = _serviceProvider.GetRequiredService<DbPgContext>();
-                return await action(context);
+                var result = await action(context);
+                if (withSave) await context.SaveChangesAsync();
+                return result;
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Ошибка в методе {method} Repository: {ex.Message} {ex.StackTrace}");
                 throw new RepositoryException($"Ошибка в методе {method} Repository: {ex.Message}");
             }
-        }
-    }
-
-    [Serializable]
-    public class RepositoryException : Exception
-    {
-        /// <summary>
-        /// default ctor
-        /// </summary>
-        public RepositoryException()
-        {
-        }
-
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="message"></param>
-        public RepositoryException(string message) : base(message)
-        {
-        }
-
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="innerException"></param>
-        public RepositoryException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        /// <summary>
-        /// ctor
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
-        protected RepositoryException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
         }
     }
 }
