d477e7d68d7dc1501463f1f05a6c3d8375546206
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2022-02-28 22:50:48 +0500

    tech task 0.0.1 works

--- App/SoftUpdater/Contract/Model/ReleaseHistory.cs
+++ App/SoftUpdater/Contract/Model/ReleaseHistory.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SoftUpdater.Contract.Model
 {
     public class ReleaseHistory : EntityHistory
     {
-        public Client Client { get; set; }
+        [Display(Name = "Каталог")]
+        public string Path { get; set; }
+        [Display(Name = "Версия")]
+        public string Version { get; set; }
+        public int Number { get; set; }
+        [Display(Name = "Клиент")]
+        public string Client { get; set; }
+        [Display(Name = "ID клиента")]
         public Guid ClientId { get; set; }
     }
 }
